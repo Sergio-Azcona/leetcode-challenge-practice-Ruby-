c8a0ea8851d2a890d0d1df93ccf6af00e7aaa473
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-05-03 10:42:52 -0400

    commented out old attempts; psudocode new attempt

--- unfinished/two_sum.rb
+++ unfinished/two_sum.rb
@@ -5,115 +5,128 @@
 # https://leetcode.com/problems/two-sum/
 
 class TwoSum
+  # def self.two_sum(nums, target)
+    # return [0,1] if nums.count == 2
 
-  def self.two_sum(nums, target)
-    return [0,1] if nums.count == 2
+    # collect = []
+    # indexes = []
 
-    collect = []
-    indexes = []
+    # # rev_nums = nums.reverse
+    # # rev_nums.pop
+    # # sorted = nums.sort
+    # #take first element and add it to the other elements in the array (cycle)
+    # #take the second element and add it to the remaining elements (not the first)., then thrid
 
-    # rev_nums = nums.reverse
-    # rev_nums.pop
-    # sorted = nums.sort
-    #take first element and add it to the other elements in the array (cycle)
-    #take the second element and add it to the remaining elements (not the first)., then thrid
+    # # sorted.map do |num|
+    # #   until num + sorted[num.next] == target
+    # #     require 'pry';binding.pry
+    # #     collect.push(num)
+    # #     collect.push(sorted[num.next])
 
-    # sorted.map do |num|
-    #   until num + sorted[num.next] == target
-    #     require 'pry';binding.pry
-    #     collect.push(num)
-    #     collect.push(sorted[num.next])
-
-      # elsif
+    #   # elsif
         
-      # require 'pry';binding.pry
-      # end
-    # end
-
-
-
-    #what nums added together == target
-      #num[0] + iterate
-      #num[1] 
-    # nums.each do |num, i|
-    #   require 'pry';binding.pry
-    #   # unless  nums[i.next] == nil
-    #     #numbers are consequitive
-    #     if num + nums[num.next] == target 
-    #       # rev_nums.map do |rn|
-    #       indexes.push(i)
-    #       indexes.push(nums[i.next])
-    #     end
+    #   # require 'pry';binding.pry
     #   # end
+    # # end
+
+
+
+    # #what nums added together == target
+    #   #num[0] + iterate
+    #   #num[1] 
+    # # nums.each do |num, i|
+    # #   require 'pry';binding.pry
+    # #   # unless  nums[i.next] == nil
+    # #     #numbers are consequitive
+    # #     if num + nums[num.next] == target 
+    # #       # rev_nums.map do |rn|
+    # #       indexes.push(i)
+    # #       indexes.push(nums[i.next])
+    # #     end
+    # #   # end
+    # # end
+
+    # nums.combination(2).find do |a, b| #find to return FIRST two that eq target and stop calculating 
+    #   if a + b == target
+    #     collect.push(a)
+    #     collect.push(b)
+    #   end
     # end
-
-    nums.combination(2).find do |a, b| #find to return FIRST two that eq target and stop calculating 
-      if a + b == target
-        collect.push(a)
-        collect.push(b)
-      end
-    end
-    # require 'pry';binding.pry
-          #numbers are not consequtive 
-          # if nums.include?(target/num)
-          #   x = target/num
+    # # require 'pry';binding.pry
+    #       #numbers are not consequtive 
+    #       # if nums.include?(target/num)
+    #       #   x = target/num
 
 
-          # nums.each do |n|
-          #   require 'pry';binding.pry
-          #     num + n == target
-          #     # collect << n
-          #     # collect  
-          #   end
-            # require 'pry';binding.pry
-          #   num + rev_nums[i] == target 
-          #   indexes.push(i)
-          #   collect.push(rev_nums)
+    #       # nums.each do |n|
+    #       #   require 'pry';binding.pry
+    #       #     num + n == target
+    #       #     # collect << n
+    #       #     # collect  
+    #       #   end
+    #         # require 'pry';binding.pry
+    #       #   num + rev_nums[i] == target 
+    #       #   indexes.push(i)
+    #       #   collect.push(rev_nums)
 
-          # unless indexes.include?(i) 
-            # if num + nums[i.next] == target && indexes.include?(i)
-              # collect.push(num)
-              # collect.push(rn)
-              # indexes.push(i)
-            # end
-            # require 'pry';binding.pry
-          # end
-      #   end
-      # end
+    #       # unless indexes.include?(i) 
+    #         # if num + nums[i.next] == target && indexes.include?(i)
+    #           # collect.push(num)
+    #           # collect.push(rn)
+    #           # indexes.push(i)
+    #         # end
+    #         # require 'pry';binding.pry
+    #       # end
+    #   #   end
+    #   # end
       
-      # end
-      # if collect.uniq == true
-      collect.uniq!
-      indexes << nums.find_index(collect.first)
-        # require 'pry';binding.pry
-      unless collect.count == 1
-        indexes << nums.index(collect.last) 
-      else
-        nums.each_with_index do |n, i|
-        # require 'pry';binding.pry
-          unless indexes.include?(i)
-            indexes << i if collect.include?(n)
-          end
-        end
-      end
+    #   # end
+    #   # if collect.uniq == true
+    #   collect.uniq!
+    #   indexes << nums.find_index(collect.first)
+    #     # require 'pry';binding.pry
+    #   unless collect.count == 1
+    #     indexes << nums.index(collect.last) 
+    #   else
+    #     nums.each_with_index do |n, i|
+    #     # require 'pry';binding.pry
+    #       unless indexes.include?(i)
+    #         indexes << i if collect.include?(n)
+    #       end
+    #     end
+    #   end
 
     
   
-      # else 
-      # require 'pry';binding.pry
-      #     require 'pry';binding.pry
+    #   # else 
+    #   # require 'pry';binding.pry
+    #   #     require 'pry';binding.pry
 
-        # nums.index(collect.first)
+    #     # nums.index(collect.first)
 
-        # indexes.include?(nums.index(collect.first))
+    #     # indexes.include?(nums.index(collect.first))
       
-      # end 
-    indexes
+    #   # end 
+    # indexes
       
-      # require 'pry';binding.pry      
+    #   # require 'pry';binding.pry      
 
-      # end
-      # indexes
+    #   # end
+    #   # indexes
+  # end
+
+  def self.two_sum(nums, target)
+
+    #if there are only two elements in nums, return the index position
+    #only 1 solution in nums (don't care about dublicates)
+
+    #hash with number => indiex
+    #if key_1 + key_2 = target, return value_1 and value_2
+
+    if nums.length == 2
+    require 'pry';binding.pry
+    end
+  
   end
 end
 
