99d3ac6c68d95f31aaf39fa60d2383a03fd1a91a
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-05-11 10:30:53 -0400

    code passing but timing out for large data

--- completed/top_k_frequent.rb
+++ completed/top_k_frequent.rb
@@ -17,7 +17,7 @@ class TopKFrequent
   
     sorted_values = num_hash.values.sort.reverse
 
-    k.times do
+    k.times do |t|
       # require 'pry';binding.pry
 
       output << num_hash.key(sorted_values[t])
@@ -26,24 +26,24 @@ class TopKFrequent
     output
   end
 
-  def  self.top_k_frequent(nums, k)
-    # Runtime 147 ms Beats 7.60%
-    # Memory 236.7 MB Beats 5.6%
+  # def  self.top_k_frequent(nums, k)
+  #   # Runtime 147 ms Beats 7.60%
+  #   # Memory 236.7 MB Beats 5.6%
 
-    num_hash = Hash.new(0)
-    output = Array.new
+  #   num_hash = Hash.new(0)
+  #   output = Array.new
 
-    nums.each do |n|
-      num_hash[n] += 1
-    end
+  #   nums.each do |n|
+  #     num_hash[n] += 1
+  #   end
 
-    k.times do
-      # require 'pry';binding.pry
-      output << num_hash.key(num_hash.values.max)
-      num_hash.delete(num_hash.key(num_hash.values.max))
-    end
-    output
-  end
+  #   k.times do
+  #     # require 'pry';binding.pry
+  #     output << num_hash.key(num_hash.values.max)
+  #     num_hash.delete(num_hash.key(num_hash.values.max))
+  #   end
+  #   output
+  # end
 end
 
 t1 = TopKFrequent.top_k_frequent([1,1,1,2,2,3], 2) #[1,2]
@@ -52,4 +52,4 @@ t3 = TopKFrequent.top_k_frequent([1,1,1,2,2,33,33,33,101,101,101,101,101,5,6,5,5
 t4 = TopKFrequent.top_k_frequent([1,2], 2) # [1,2]
 t5 = TopKFrequent.top_k_frequent([44,44,3,22,44,1,22,44,22], 2) #[44,22]
 
-# require 'pry';binding.pry
+require 'pry';binding.pry
---
+++ unfinished/array_except_self.rb
@@ -0,0 +1,54 @@
+# https://leetcode.com/problems/product-of-array-except-self/
+
+# 238. Product of Array Except Self
+# Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+# You must write an algorithm that runs in O(n) time and without using the division operation.
+
+class ArrayExceptSelf
+  def self.product_except_self(nums) #passes but times out in leetcode
+    
+    # unless nums.
+    # # require 'pry';binding.pry
+    # product_array = Array.new
+    # nums.length.times do
+    #   num = nums.shift
+    #   product_array.push(nums.inject(:*))
+    #   nums.push(num)
+    # end
+    # product_array
+
+
+    product_array = []
+    product = 1
+    nums.each { |num| product *= num }
+    nums.each do |num| 
+      unless num != 0
+        next
+      else
+        product_array << product / num 
+      end
+    end
+    product_array
+
+  end
+
+  # def self.product_except_self(nums)
+  #   product_array = Hash.new
+      
+  #   nums.length.times do |i|
+  #     # require 'pry';binding.pry
+  #     num = nums.shift
+  #     product_array[i] = nums.inject(:*)
+  #     nums.push(num)
+  #   end
+  #   product_array.values
+  # end
+end
+
+# t1 = ArrayExceptSelf.product_except_self([1,2,3,4]) #  [24,12,8,6]
+t2 = ArrayExceptSelf.product_except_self([-1,1,0,-3,3]) #[0,0,9,0,0]
+# t3 = ArrayExceptSelf.product_except_self([0,0]) # [0,0]
+# t1 = ArrayExceptSelf.product_except_self()
+
+require 'pry';binding.pry
