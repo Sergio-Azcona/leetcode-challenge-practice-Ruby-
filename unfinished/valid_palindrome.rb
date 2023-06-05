74c395432617673ac5fde0342ed7fdc325388833
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-06-05 13:18:57 -0400

    MinStack: passed; ValidPalidrome: passed - a two pointer q

--- unfinished/valid_palindrome.rb
+++ unfinished/valid_palindrome.rb
@@ -6,26 +6,51 @@
 # https://leetcode.com/problems/valid-palindrome/
 
 class ValidPalindrom
-  def self.is_palindrome(s)
-    
-    #downcase s
-    #remove any special characters
-    clean_s = s.downcase.gsub(/[0-9A-Za-z]/)
-    clean_s.reverse_each ==  clean_s
-    #collect all values in revese order
-    reversed_clean_s = Array.new
-    origin_order_s =  Array.new
+  # def self.is_palindrome(s) #does not pass leetcode time constrain
+  #   #downcase s
+  #   #remove any special characters
+  #   clean_s = s.downcase.gsub(/[0-9A-Za-z]/)
+  #   clean_s.reverse_each ==  clean_s
+  #   #collect all values in revese order
+  #   reversed_clean_s = Array.new
+  #   origin_order_s =  Array.new
 
-    origin_order_s.key("A")
+  #   # origin_order_s.key("A")
 
-    # require 'pry';binding.pry
-    clean_s.each_byte do |c|
-      reversed_clean_s.unshift(c) #put letter in index[0] [3,2,1,0]
-      origin_order_s.push(c) #append the letter [0,1,2,3] <<
+  #   # require 'pry';binding.pry
+  #   clean_s.each do |c|
+  #     reversed_clean_s.unshift(c) #put letter in index[0] [3,2,1,0]
+  #     origin_order_s.push(c) #append the letter [0,1,2,3] <<
+  #   end
+  #   # evalate if the two strings match
+  #   # require 'pry';binding.pry
+  #   reversed_clean_s  == origin_order_s
+  # end
+
+  def self.is_palindrome(s) #fast but memory intensive. passes leetcode time
+    # Runtime 103 ms Beats 80.43%
+    # Memory 217.1 MB Beats 18.1%
+
+    if s.class == String
+      new_s = s.gsub(/[^0-9a-zA-Z]/, '').downcase.bytes #converting string to bytes allows us to compare integers rather than characters 
+    elsif
+      s.class == Array
+      new_s = s
     end
-    # evalate if the two strings match
-    # require 'pry';binding.pry
-    reversed_clean_s  == origin_order_s
+
+    if  new_s.length > 1 
+      if new_s.shift == new_s.pop
+        is_palindrome(new_s)
+      else
+        return false
+      end
+    elsif
+      new_s.length <= 1 #new_s
+      return true
+    end
+  end
+
+  def self.compare_two_points(array)
   end
 end
 
@@ -34,7 +59,7 @@ t2 = ValidPalindrom.is_palindrome("I have 1 thing to do 2day") #fasle
 t3 = ValidPalindrom.is_palindrome("I-4-I") #true 
 t4 = ValidPalindrom.is_palindrome("1234321") #true 
 t5 = ValidPalindrom.is_palindrome("") #true 
-t6 = ValidPalindrom.is_palindrome("a") #true
-t7 = ValidPalindrom.is_palindrome("race a car") #false
-
+t6 = ValidPalindrom.is_palindrome("a.") #true
+t7 = ValidPalindrom.is_palindrome("A man, a plan, a canal: Panama") #true
+t8 = ValidPalindrom.is_palindrome("race a car") # false
 require 'pry';binding.pry
